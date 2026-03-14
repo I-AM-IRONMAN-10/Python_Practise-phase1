5a0efa377bfeed2863aac72d718e7248d07833de
I-AM-IRONMAN-10 <23aids044@act.edu.in>
2026-03-14 15:59:19 +0530

    problem 1

--- Two_sum_problem.py
+++ Two_sum_problem.py
@@ -1,9 +1,12 @@
+
+#14.03.2026 question -1
+
 class Solution(object):
     def twoSum(self, nums, target):
         d={}
         for i,n in enumerate(nums):
             if target-n in d:
-                return [d[target-n], i]
+                return [d[target-n],i]
             d[n]=i
 
 nums=list(map(int,input().split()))
